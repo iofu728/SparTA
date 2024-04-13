8ea21be9a010a82c5d4b4a7fa30e45208953509b
Huiqiang Jiang <iofu728@163.com>
2024-04-13 02:55:43 +0000

    Fix(PIT): fix the blob link

--- script/figure10/run.sh
+++ script/figure10/run.sh
@@ -5,7 +5,6 @@ conda activate artifact
 
 pip install git+https://github.com/iofu728/transformers.git@v4.20.0_PIT
 
-azcopy copy "https://australiav100data.blob.core.windows.net/hjiang/Experiments/DynamicSparse/data.zip?sv=2021-04-10&st=2023-07-25T06%3A39%3A46Z&se=2024-07-26T06%3A39%3A00Z&sr=b&sp=r&sig=L2vyu71YJMTvnSryNHKgJHGcO1IP9O8a3yyS6wVA05g%3D" ./ --recursive
 unzip data.zip
 
 DATA_DIRS=(data/glue data/LongDocument)
--- script/figure11/run.sh
+++ script/figure11/run.sh
@@ -9,7 +9,6 @@ wget https://ai2-s2-research.s3-us-west-2.amazonaws.com/longformer/longformer-la
 tar -zxvf longformer-base-4096.tar.gz
 tar -zxvf longformer-large-4096.tar.gz
 
-azcopy copy "https://australiav100data.blob.core.windows.net/hjiang/Experiments/DynamicSparse/data.zip?sv=2021-04-10&st=2023-07-25T06%3A39%3A46Z&se=2024-07-26T06%3A39%3A00Z&sr=b&sp=r&sig=L2vyu71YJMTvnSryNHKgJHGcO1IP9O8a3yyS6wVA05g%3D" ./ --recursive
 unzip data.zip
 cp data/*.pkl ./
 
